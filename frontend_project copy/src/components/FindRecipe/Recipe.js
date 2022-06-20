822843b0c749731c2e1761c7160fb1eaed527ad7
Mani <marhm005@gmail.com>
2022-06-20 09:53:20 +0100

    added material UI

---
+++ frontend_project copy/src/components/FindRecipe/Recipe.js
@@ -0,0 +1,5 @@
+const Recipe = ({ recipe }) => {
+//   return <h2>{recipe.name}</h2>;
+};
+
+export default Recipe;
---
+++ frontend_project copy/src/components/FindRecipe/RecipeSearch.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./searchbar.css";
+import Button from "@mui/material/Button";
+import "./flexbox.css";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+const RecipeSearch = ({ recipes }) => {
+  const [query, setQuery] = useState("");
+
+  const inputHandler = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const checker = recipes
+    .filter((recipe) => {
+      if (query === "") {
+        return recipe;
+      } else if (recipe.name.toLowerCase().includes(query.toLowerCase())) {
+        return recipe;
+      }
+    })
+    .map((recipe) => {
+      return (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <Button variant="contained" color="primary">
+            Details
+          </Button>
+        </div>
+      );
+    });
+
+  return (
+    <>
+      <div className="searchBar-wrap">
+        <SearchIcon className="searchBar-icon" />
+        <input type="text" placeholder="Momo" onChange={inputHandler} />
+      </div>
+      {checker}
+    </>
+  );
+};
+
+export default RecipeSearch;
